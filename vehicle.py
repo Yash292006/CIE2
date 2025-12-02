320c4179fecd8ff7c00ad2bf5301073bec924d6a
dhruva7vernekar-blip <dhruva77vernekar@gmail.com>
2025-12-02 11:47:16 +0530

    save

---
+++ vehicle.py
@@ -0,0 +1,20 @@
+# employee.py
+
+def vehicle_details(no, name, type, year):
+    result = (
+        f"Vehical Number: {no}\n"
+        f"Owner Name: {name}\n"
+        f"Vehicle Type: {type}\n"
+        f"Year of Manufacture: {year}"
+    )
+    return result
+
+
+if __name__ == "__main__":
+    # Sample input
+    no = "KA251498"
+    name = "Yash"
+    type = "bike"
+    year = "2013"
+
+    print(vehicle_details(no, name, type, year))
